3305c938b41df08bfb4ab5348b98e6ee16a33497
dbowling21 <davidbowling21@gmail.com>
2022-04-15 18:42:25 -0600

    combined checking for start of a comment into one if statement rather than two

--- src/CommentSearch.java
+++ src/CommentSearch.java
@@ -6,7 +6,7 @@ public class CommentSearch {
 	
 	public static void main(String[] args) throws IOException {
 		
-		boolean chooseFile = true; // toggle whether to display GUI file explorer or hardcode
+		boolean chooseFile = false; // toggle whether to display GUI file explorer or hardcode
 		File cFile;
 		if (chooseFile){
 			JFileChooser fileGUI = new JFileChooser();
@@ -30,25 +30,23 @@ public class CommentSearch {
 		while((c = br.read()) != -1) //loop until end of file
 		{
 			character = (char) c;
-			if (String.valueOf(character).equals("/")){ //potential start of comment
-				character = (char) br.read();
-				if (String.valueOf(character).equals("*")){ //start of comment
-					comment.append("/*");
-					count ++;
-					do{
-						character =(char) br.read();
-						comment.append(character); //add chars to comment until comment escape
-						
-						//checks if length of comment is greater than 3 for a case where "/*/" would
-						// otherwise be recognized as a comment
-					} while (!(String.valueOf(comment.charAt(comment.length() -2)).equals("*")
-							   && String.valueOf(character).equals("/") && comment.length() > 3));
-								
-					System.out.println("Comment " + count);
-					System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-					System.out.println(comment + "\n");
-					comment.delete(0, comment.length()); //reset comment
-				}
+			if (String.valueOf(character).equals("/") && String.valueOf((char) br.read()).equals("*")){ //start of comment
+				comment.append("/*");
+				count ++;
+				do{
+					character =(char) br.read();
+					comment.append(character); //add chars to comment until comment escape
+
+					//checks if length of comment is greater than 3 for a case where "/*/" would
+					// otherwise be recognized as a comment
+				} while (!(String.valueOf(comment.charAt(comment.length() -2)).equals("*")
+						   && String.valueOf(character).equals("/") && comment.length() > 3));
+
+				System.out.println("Comment " + count);
+				System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+				System.out.println(comment + "\n");
+				comment.delete(0, comment.length()); //reset comment
+
 			}
 		}
 	}
